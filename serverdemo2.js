e467d6374e9da403d2f16f013d6fa86bdf5256f9
Batli Joselevitz <batlijoselevitz@Batlis-MacBook-Pro.local>
2017-12-21 20:19:17 -0600

    node modules

---
+++ serverdemo2.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var port = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //request for post to parse back whatever the body is to be able to use it
+// all requests and responses will have headers and will always have the same format
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "santal33!",
+  database: "task_saver_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Root get route
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM tasks;", function(err, data) {
+    if (err) throw err;
+
+
+    // Test it
+    // console.log('The solution is: ', data);
+
+    // Test it
+    // return res.send(data);
+
+    res.render("index", { tasks: data });
+  });
+});
+
+// Post route -> back to home
+app.post("/", function(req, res) {
+  // Test it
+  // console.log('You sent, ' + req.body.task);
+
+  // Test it
+  // return res.send('You sent, ' + req.body.task);
+
+  connection.query("INSERT INTO tasks (task) VALUES (?)",
+  [req.body.task], function(err, result) {
+    if (err) throw err;
+
+    res.redirect("/"); //redirect to the route directory
+  });
+});
+
+app.listen(port);
