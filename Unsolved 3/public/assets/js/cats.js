d2928034ea2bbfe8c758207bfae3fa3c719738b2
Batli Joselevitz <batlijoselevitz@Batlis-MacBook-Pro.local>
2018-01-04 21:03:47 -0600

    last class activity

---
+++ Unsolved 3/public/assets/js/cats.js
@@ -0,0 +1,60 @@
+/*
+Add a delete button into the index.handlebars file next to each cat.
+Add on to the following:
+The cats.js file to add a jQuery event handler for the delete button.
+The ORM to include a delete key and function
+The cat model to include a delete key and function that uses the ORM
+The catsController.js file to have a /api/cats/:id delete route,
+to call the delete key of the cat model, and to pass in arguments as necessary
+*/
+
+
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-sleep").on("click", function(event) {
+    var id = $(this).data("id");
+    var newSleep = $(this).data("newsleep");
+
+    var newSleepState = {
+      sleepy: newSleep
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/cats/" + id, {
+      type: "PUT",
+      data: newSleepState
+    }).then(
+      function() {
+        console.log("changed sleep to", newSleep);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newCat = {
+      name: $("#ca").val().trim(),
+      sleepy: $("[name=sleepy]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/cats", {
+      type: "POST",
+      data: newCat
+    }).then(
+      function() {
+        console.log("created new cat");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
+
+
+$("button").click(function(){ $("p").remove();
